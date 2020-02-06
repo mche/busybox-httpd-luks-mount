a29667075cb0cef5deafad27574a571ecceaf5f8
Mikhail Che <mche@cpan.org>
2020-02-06 10:55:03 +0500

    дал

--- cgi-bin/config.sh
+++ cgi-bin/config.sh
@@ -13,5 +13,5 @@ export mountPoint=/mnt/iso
 # Если нужно убрать подальше в сеть luks-key.sh и luks-mount.sh, тогда локально их удалить и раскомментировать и указать baseURL для них
 #export baseURL=https://raw.githubusercontent.com/mche/busybox-httpd-luks-mount/master/cgi-bin
 
-# Укаазать, где сохранена первая часть ключа
+# Указать, где сохранена первая часть ключа
 export key1URL=https://gist.githubusercontent.com/mche/3894cedc3997e3acd97470c63bf9ba4a/raw/key.txt
